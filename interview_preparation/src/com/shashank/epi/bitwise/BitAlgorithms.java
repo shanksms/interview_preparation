392cbb7b8d300597b275ee62e277e4b02f8d04d8
pooja <sriv.srivastava@gmail.com>
2017-09-16 17:21:00 +0530

    committing

--- interview_preparation/src/com/shashank/epi/bitwise/BitAlgorithms.java
+++ interview_preparation/src/com/shashank/epi/bitwise/BitAlgorithms.java
@@ -1,17 +1,11 @@
 package com.shashank.epi.bitwise;
 
 class BitAlgorithms {
-    public static void main(String[] args) {
-    	int number = 7;
-    	
-    	System.out.println(new BitAlgorithms().countSetBits(number));
-
-    }
-    
+   
     
     public int countSetBits(int number) {
     	int count = 0;
-    	while (number > 0) {
+    	while (number != 0) {
     		count = count + (number & 1);
     		//System.out.println(Integer.toBinaryString(number));
     		number = number >>> 1;
@@ -19,6 +13,18 @@ class BitAlgorithms {
     	
     	
     	return count;
+    }
+    
+    public short computeBitParity(long number) {
+    	short parity = 0;
+    	while (number != 0) {
+    		parity ^= (number & 1);
+    		number = number >>> 1;
+    	}
+    	
+    	
+    	
+    	return parity;
     }
     
     
--- interview_preparation/src/com/shashank/epi/bitwise/TestBitAlgorithms.java
+++ interview_preparation/src/com/shashank/epi/bitwise/TestBitAlgorithms.java
@@ -13,20 +13,27 @@ import org.junit.Test;
  *
  */
 
-public class TestCountSetBits {
-	private CountSetBits countSetBits;
+public class TestBitAlgorithms {
+	private BitAlgorithms bitAlgorithms;
 	
 	@Before
 	public void before() {
-		countSetBits = new CountSetBits();
+		bitAlgorithms = new BitAlgorithms();
 	}
 	
 	
 	@Test
 	public void testCountBits() {
-		//Binary Rep 11
+		//Binary Rep 10
 		int number = 4;
-		assertEquals(2, countSetBits.countSetBits(number));
+		assertEquals(1, bitAlgorithms.countSetBits(number));
 		
+	}
+	
+	@Test
+	public void testComputeBitParity() {
+		//bit parity of 1 is 1
+		long number = 1;
+		assertEquals(1, bitAlgorithms.computeBitParity(number));
 	}
 }
